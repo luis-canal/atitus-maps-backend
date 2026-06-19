fb3eeb75555e41c0bed785881999c9a634bba565
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-06-18 21:39:46 -0300

    implementa os métodos get e post no endpoint wspoint

--- src/main/java/br/edu/atitus/luis_canal/apisample/controllers/PointController.java
+++ src/main/java/br/edu/atitus/luis_canal/apisample/controllers/PointController.java
@@ -1,8 +1,13 @@
 package br.edu.atitus.luis_canal.apisample.controllers;
 
+import br.edu.atitus.luis_canal.apisample.dtos.PointDTO;
+import br.edu.atitus.luis_canal.apisample.entities.PointEntity;
 import br.edu.atitus.luis_canal.apisample.services.PointService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/ws/point")
@@ -13,5 +18,25 @@ public class PointController {
 
     public PointController(PointService service) {
         this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PointEntity>> findAll() {
+        var lista = service.findAll();
+        return ResponseEntity.ok(lista);
+    }
+
+    @PostMapping
+    public ResponseEntity<PointEntity> save(@RequestBody PointDTO dto) throws Exception {
+        PointEntity point = new PointEntity();
+        BeanUtils.copyProperties(dto, point);
+        service.save(point);
+        return ResponseEntity.status(201).body(point);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> exceptionHandler(Exception ex) {
+        String message = ex.getMessage().replace("\r\n", "");
+        return ResponseEntity.badRequest().body(message);
     }
 }
